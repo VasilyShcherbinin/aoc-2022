08bcaa97ef09bff963d7bb91efc0f2509fb8ed9a
Vasily Shcherbinin <vasily.shcherbinin@outlook.com>
2022-12-10 00:36:10 +0000

    Add files via upload

---
+++ Day6/aocd6part2.rs
@@ -0,0 +1,28 @@
+use std::{io, path::Path, fs};
+
+fn main() -> io::Result<()> {
+
+    let path = Path::new("file1.txt");
+    let contents = fs::read_to_string(path).expect("Failed to read file");
+    let char_vec: Vec<char> = contents.chars().collect();
+    let mut answer = 0;
+    let window_size = 14;
+
+    let iter = char_vec.windows(window_size);
+    let iter_slice_vec = iter.collect::<Vec<_>>();
+    for (index,vec_slice) in iter_slice_vec.iter().enumerate() {
+        let mut result_vec = vec_slice.to_vec();
+        result_vec.sort();
+        result_vec.dedup();
+        if result_vec.len() == vec_slice.len() {
+            println!("Found unique result slice {:?}, index {}", vec_slice, index);
+            answer = index+window_size;
+            break;
+        }
+    }
+
+    println!("{}",answer);
+
+    Ok(())
+}
+
